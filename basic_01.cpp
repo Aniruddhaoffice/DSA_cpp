434d09a72c1597b2b7dd9766039433d1566d2621
Aniruddhaoffice <144651519+Aniruddhaoffice@users.noreply.github.com>
2025-03-20 00:48:04 +0530

    initial commit 20-03-2025 00:47 am

---
+++ basic_01.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+
+    /*cout << "Hello, World!" << endl;
+    return 0;
+
+   
+
+
+    int  n; 
+    cin>>n;
+
+    int i = 1; 
+
+
+    while (i<=n)
+    {
+        cout << i << endl;
+        i++;
+    }
+        
+    
+  
+    
+    int sum = 0;
+    int i = 1 ; 
+    while (i<=15)
+    {
+        cout << sum + i << endl;
+        i++;
+    }
+
+    
+
+    int n ;
+
+    cin>>n;
+
+    int i = 1;
+    int sum = 0;
+    while (i<=n)
+    {
+        sum = sum + i;
+        i++;
+    }
+    cout << sum << endl;
+    return 0;
+
+    
+
+    int n;
+    cin >> n;
+
+    int i = 2;
+    int sum = 0 ;
+
+    while (i<=n)
+    {
+      sum = sum + i;
+        i = i + 2;
+
+    }
+    cout << sum << endl;
+    return 0;
+
+    
+
+    int n;
+    cin >> n;
+
+    int i = 2;
+
+    while (i<n)
+    {
+        if (n%i == 0)
+        {
+            cout << "Not Prime for "<< i << endl;
+            return 0;
+        }else
+        {
+            cout << "Prime for " << i << endl;
+        }
+        i = i+1;
+    }
+    */
+
+    
+
+    
+}
---
+++ patternt.cpp
@@ -0,0 +1,139 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+/*
+
+//pattern 1
+
+
+    int n ;
+    cin >> n ;
+
+    int i = 1 ;
+
+    while (i<=n)
+    {
+    int j = 1;
+      while (j<=n)
+      {
+            cout << "*";
+            j++;
+      }
+      cout << endl;
+      i++;
+    }
+
+
+
+//pattern 3
+
+    int n ;
+    cin >> n ;
+
+    int i = 1 ;
+
+    while (i<=n)
+    {
+    int j = 1;
+      while (j<=n)
+      {
+            cout << j;
+            j = j + 1;
+      }
+      cout << endl;
+      i = i + 1;
+    }
+   
+
+//pattern 2
+
+
+int n ;
+cin >> n ;
+
+int i = 1 ;
+
+while (i<=n)
+{
+int j = 1;
+  while (j<=n)
+  {
+        cout << i ;
+        j = j + 1;
+  }
+  cout << endl;
+  i = i + 1;
+}
+
+
+
+
+//Basic hand practice on c++
+
+//problem no 1 
+
+int a ;
+int b;
+cin >> a ;
+cin >> b ;
+
+
+int sum = a + b ;
+
+if (a>b){
+    cout << "A is greater "<<endl;
+}else {
+    cout <<"b is greater then A" <<endl;
+}
+
+
+
+//problem no 2 
+
+int x ;
+
+
+cin >> x;
+
+
+int i = 1  ; 
+
+
+for (i = 1 ; i <= x ; i = i + 1 ){
+    
+    if (i % x == ! 0 ){
+
+        cout<< i <<endl;
+    }
+
+}
+
+*/
+
+
+//problem no 3
+
+int n ;
+cin >> n ;
+
+int i = 1 ;
+
+
+while (i<=n)
+{
+    int j = 1;
+    while (j<=n)
+    {
+        cout << "*"; ;
+        j ++;
+    }
+    cout << endl;
+    i++;
+}
+
+    return 0;
+    
+    
+}
