2375f72f4321bef12c5518e8df46d1dc319e1703
Aniruddhaoffice <144651519+Aniruddhaoffice@users.noreply.github.com>
2025-04-08 14:36:05 +0530

    initial 08-03-25

--- Function/function.cpp
+++ Function/function.cpp
@@ -27,6 +27,26 @@ using namespace std;
 
  bool isEven(int a){
 
-    if(a&1)
+    if(a&1){
+
+        return 0;
+
+    }else{
+        return 1;
+    }
 
  }
+
+ int main(){
+    int num ;
+    cin >> num ;
+  if( isEven(num)){
+    cout << "number is even" << endl;
+  }else{
+    cout << "number is odd" << endl ;
+  }
+   
+ }
+
+ 
+
